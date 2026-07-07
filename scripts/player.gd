d7c7f39ef201bd32012616e9b788b25eca8e9b4c
callumrann <callumrann@gmail.com>
2026-07-07 20:41:14 +1000

    fix: lock player movement direction (only one button -> jump)

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,22 +1,22 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const MOVEMENT_SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+
+const LEFT: int = -1
+const RIGHT: int = 1
+
+var direction: int = RIGHT
 
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
-		velocity += get_gravity() * delta # gravity apparently from project settings
+		velocity += get_gravity() * delta # gravity changed in project settings
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("move_left", "move_right") # No movement during actual jam
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	velocity.x = direction * MOVEMENT_SPEED
 
 	move_and_slide()
